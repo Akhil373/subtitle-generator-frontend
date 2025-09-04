786d9233eb416d047914a03501fb3992233fb166
Akhil <133588800+Akhil373@users.noreply.github.com>
2025-09-04 08:16:56 +0530

    fix: github repo URL

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,7 @@
 const TopBar = () => {
   return (
     <section className='flex animate-[spin_1s] w-[44px] absolute top-[16px] right-[16px]'>
-      <a href='https://github.com/Akhil373/subtitle-generator-backend' target='_blank'>
+      <a href='https://github.com/Akhil373/sub-gen-aws' target='_blank'>
         <img src="github-logo.svg" alt="github URL" />
       </a>
     </section>
